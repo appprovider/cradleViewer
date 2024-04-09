5d0fd8e8d5bcaca1ba616a0d930c1a8bed491dbe
陆兆龙 <luzhaolong@sjkj.com>
2024-04-09 18:32:39 +0800

    socket

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, json
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="http://127.0.0.1:5173")
 
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/para/projector')
 def test_connect():
-    emit('response', {'data': 'Connected'})
+    emit('response', {'data': 'Projector backend connected'})
 
 
-@socketio.on('request', namespace='/test')
+@socketio.on('request', namespace='/para/projector')
 def handle_request(message):
+    data = json.loads(message['data'])
+    print(data)
     emit('response', {'data': 'yes you got data'})
 
 
