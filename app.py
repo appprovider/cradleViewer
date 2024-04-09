60c311f0abe720c98ee1c0954acfacd41c4fb79a
陆兆龙 <luzhaolong@sjkj.com>
2024-04-09 11:57:25 +0800

    test socket

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from cradleViewer import app
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="http://127.0.0.1:5173")
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('response', {'data': 'Connected'})
+
+
+@socketio.on('request', namespace='/test')
+def handle_request(message):
+    emit('response', {'data': 'yes you got data'})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
--- cradleViewer/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from cradleViewer.module1.routes import mod as module1
-
-app = Flask(__name__)
-app.register_blueprint(module1)
